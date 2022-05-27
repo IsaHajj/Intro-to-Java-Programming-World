c1c2335f7996e3580a8c9e77ad2400d8e7dd2d72
IsaHajj <abdulpr3@gmail.com>
2022-05-27 01:12:47 +0100

    first commit

--- Exercises1/Exercise1_12.java
+++ Exercises1/Exercise1_12.java
@@ -1,5 +1,5 @@
 /**
-(Average speed in kilometers) Assume a runner runs 24 miles in 1 hour, 40     minutes, and 35 seconds.
+(Average speed in kilometers) Assume a runner runs 24 miles in 1 hour, 40 minutes, and 35 seconds.
 Write a program that displays the average speed in kilometers per hour.
 (Note that 1 mile is 1.6 kilometers)
 */
