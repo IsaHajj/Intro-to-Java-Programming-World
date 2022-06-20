164cbfbe09ece6c21e21d9e83d87f831e57071f5
IsaHajj <abdulpr3@gmail.com>
2022-06-20 02:11:26 +0100

    first commit

--- Exercises2/Exercise2_01.java
+++ Exercises2/Exercise2_01.java
@@ -1,24 +1,25 @@
-/**
-(Convert mile to kilometer) Write a program that reads a mile in a double value
-from the console, converts it to kilometers, and displays the result. The formula
-for the conversion is as follows:
-1 mile = 1.6 kilometers
+/*
+(Convert Celsius to Fahrenheit) Write a program that reads a Celsius degree in
+a double value from the console, then converts it to Fahrenheit and displays the
+result. The formula for the conversion is as follows:
+fahrenheit = (9 / 5) * celsius + 32
 */
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class Exercise2_01 {
-   public static void main(String[] args) {
-      // Create a Scanner object
-      Scanner input = new Scanner(System.in);
-      
-      // Prompt the user to enter miles
-      System.out.print("Enter miles: ");
-      double miles = input.nextDouble();
-      
-      // Compute kilometers
-      double kilometers = miles * 1.6;
-      
-      // Displays results
-      System.out.println((int)miles + " miles is " + (int)(kilometers * 100) / 100.0 + " kilometers");
-   }
+    public static void main(String[] args) {
+       // Create new Scanner object
+       Scanner input = new Scanner(System.in);
+
+       // Prompt user to input a double
+       System.out.print("Enter a degree in Celsius: ");
+       double celsius = input.nextDouble();
+
+       // Convert Celsius to Fahrenheit
+       double fahrenheit = 9.0 / 5 * celsius + 32;
+
+       // Display results
+       System.out.println(celsius + " Celsius is " + fahrenheit
+	  + " Fahrenheit"); 
+    }
 }
