4b6386a7ade28ca676be5530165652ec5858a691
IsaHajj <abdulpr3@gmail.com>
2022-08-06 22:52:22 +0100

    first commit

--- Exercises2/Exercise2_07.java
+++ Exercises2/Exercise2_07.java
@@ -6,19 +6,18 @@ For simplicity, assume a year has 365 days. Here is a sample run:
 import java.util.Scanner;
 
 public class Exercise2_07 {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
-		// Prompt the user to enter the number of minutes
-		System.out.print("Enter the number of minutes: ");
-		int minutes = input.nextInt();
+    // Prompt the user to enter the number of minutes
+    System.out.print("Enter the number of minutes: ");
+    int minutes = input.nextInt();
 
-		// Compute the number of years and days
-		int years = minutes / 525600;
-		int days = (minutes % 525600) / 1440;
+    // Compute the number of years and days
+    int years = minutes / 525600;
+    int days = (minutes % 525600) / 1440;
 
-		// Display results
-		System.out.println(minutes + " minutes is approximately " + years
-			+ " years and " + days + " days");
-	}
+    // Display results
+    System.out.println(minutes + " minutes is approximately " + years + " years and " + days + " days");			
+    }
 }
