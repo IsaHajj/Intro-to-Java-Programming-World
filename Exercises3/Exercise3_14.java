c74b5f5bf85c30e9493e987f9f32766952b120d4
IsaHajj <abdulpr3@gmail.com>
2022-09-20 11:55:07 +0100

    Modified comment

--- Exercises3/Exercise3_14.java
+++ Exercises3/Exercise3_14.java
@@ -1,7 +1,14 @@
+/*
+(Game: heads or tails) Write a program that lets the user guess whether the flip of
+a coin results in heads or tails. The program randomly generates an integer 0 or 1,
+which represents head or tail. The program prompts the user to enter a guess and
+reports whether the guess is correct or incorrect.
+*/
+
 import java.util.Random;
 public class Exercise3_14 {
     public static void main(String[] args) {
-        // Create a Scanner
+        //Import & Create a Scanner
         java.util.Scanner hp = new java.util.Scanner(System.in);
         Random random = new Random();
         
