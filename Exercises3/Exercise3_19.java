5d778ab41dbc5da4cfe54b2fb9d4aad2630722ae
IsaHajj <abdulpr3@gmail.com>
2022-09-28 23:31:31 +0100

    Modified

--- Exercises3/Exercise3_19.java
+++ Exercises3/Exercise3_19.java
@@ -1,31 +1,41 @@
+/*
+(Compute the perimeter of a triangle) Write a program that reads three edges for
+a triangle and computes the perimeter if the input is valid. Otherwise, display that
+the input is invalid. The input is valid if the sum of every pair of two edges is
+greater than the remaining edge.
+*/
+
 public class Exercise3_19 {
      public static void main(String[] args) {
-        // Create a Scanner
-        java.util.Scanner input = new java.util.Scanner(System.in);
+       // import and Create a Scanner
+       java.util.Scanner input = new java.util.Scanner(System.in);
         
-        // Prompts user to enter three edges for triangle
-        System.out.println("Enter the three edges for triangle: ");
-        System.out.print("Edges 1: ");
-        double edges1 = input.nextDouble();
+      // Prompts user to enter three edges for triangle
+      System.out.println("Enter three edges for a triangle:");
+      System.out.print(" Edge 1 points x, y: ");
+      double x1 = input.nextDouble();
+      double y1 = input.nextDouble();
+      System.out.print(" Edge 2 points x, y: ");
+      double x2 = input.nextDouble();
+      double y2 = input.nextDouble();
+      System.out.print(" Edge 3 points x, y: ");
+      double x3 = input.nextDouble();
+      double y3 = input.nextDouble();
         
-        System.out.print("Edges 2: ");
-        double edges2 = input.nextDouble();
+      // Test if input is valid
+      boolean validate = (x1 + y1 > x3 + y3 && x2 + y2 > x3 + y3) || (x1 + y1 > x2 + y2 && x3 + y3 > x2 + y2) || (x3 + y3 > x1 + y1 && x2 + y2 > x1 + y1);
+							 
         
-        System.out.print("Edges 3: ");
-        double edges3 = input.nextDouble();
-        
-        // Compute the perimeter and dislpay result
-        if (edges1 + edges2 > edges3 && edges1 + edges3 > edges2 && edges2 + edges3 > edges1) {
-            System.out.println("Perimeter is = " + (edges1 + edges2 + edges3));
-        } else 
-            System.out.println("the Input is Invalid.");
-        /*    
-        OR  // Compute the perimeter and dislpay result
-        if (edges1 < edges2 + edges3 && edges2 < edges1 + edges3 && edges3 < edges2 + edges1) {
-        double perimeter = edges1 + edges2 + edges3;
-            System.out.println("Perimeter is = " + perimeter);
-        } else 
-            System.out.println("the Input is Invalid. ");
-       */
-    }
+      if (!validate) {
+	  System.out.println("Input is invalid.");
+	  System.exit(1);
+      }
+      // Compute the sides of the triangle
+      double side1 = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
+      double side2 = Math.pow(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2), 0.5);
+      double side3 = Math.pow(Math.pow(x1 - x3, 2) + Math.pow(y1 - y3, 2), 0.5);
+
+      // Display the perimeter of the triangle
+      System.out.println("perimeter of triangle is " + (side1 + side2 + side3));
+      }
 }
