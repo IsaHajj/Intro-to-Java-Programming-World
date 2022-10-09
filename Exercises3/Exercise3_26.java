4c22734a2f46e7914a5f2a455d132d4833070d0e
IsaHajj <abdulpr3@gmail.com>
2022-10-10 00:04:45 +0100

    First commit

--- Exercises3/Exercise3_26.java
+++ Exercises3/Exercise3_26.java
@@ -3,7 +3,6 @@
 an integer and determines whether it is divisible by 5 and 6, whether it is divisible
 by 5 or 6, and whether it is divisible by 5 or 6, but not both. Here is a sample run
 of this program: 
-whether it is divisible by 5 or 6, but not both. He
 */
 
 public class Exercise3_26 {
