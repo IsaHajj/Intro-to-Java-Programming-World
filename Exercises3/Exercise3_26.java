7a3e14f01751c79cc6a5ee65d3dbe2aa2bd63c26
IsaHajj <abdulpr3@gmail.com>
2022-10-09 23:55:32 +0100

    First commit

--- Exercises3/Exercise3_26.java
+++ Exercises3/Exercise3_26.java
@@ -2,7 +2,8 @@
 (Use the &&, || and ^ operators) Write a program that prompts the user to enter
 an integer and determines whether it is divisible by 5 and 6, whether it is divisible
 by 5 or 6, and whether it is divisible by 5 or 6, but not both. Here is a sample run
-of this program:
+of this program: 
+whether it is divisible by 5 or 6, but not both. He
 */
 
 public class Exercise3_26 {
