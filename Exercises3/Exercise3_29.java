765f090800f5f31651e99208cedcb965e46a40e8
IsaHajj <abdulpr3@gmail.com>
2022-10-17 23:58:46 +0100

    First commit

--- Exercises3/Exercise3_29.java
+++ Exercises3/Exercise3_29.java
@@ -2,7 +2,7 @@
 (Geometry: two circles) Write a program that prompts the user to enter the center
 coordinates and radii of two circles and determines whether the second circle is
 inside the first or overlaps with the first, as shown in Figure 3.10. (Hint: circle2 is
- 6 = |r1 - r2| and circle2
+inside circle1 if the distance between the two centers 6 = |r1 - r2| and circle2
 overlaps circle1 if the distance between the two centers <= r1 + r2. Test your
 program to cover all cases.)
 */
