9dc3c160dc278c95813d656892967df446542f34
IsaHajj <abdulpr3@gmail.com>
2022-11-08 23:58:29 +0100

    First commit

---
+++ Exercises4/Exercise4_09.java
@@ -0,0 +1,20 @@
+/*
+(Find the Unicode of a character) Write a program that receives a character and
+displays its Unicode.
+*/
+import java.util.Scanner;
+
+public class Exercise4_09 {
+   public static void main(String[] args) {
+     // Creata a Scanner
+     Scanner input = new Scanner(System.in);
+    
+     // Prompt the user to enter a character
+     System.out.print("Enter a character: ");
+     String s = input.nextLine();
+     char ch = s.charAt(0);
+
+     // Display Unicode of character
+     System.out.println((int)ch);
+  }
+}
