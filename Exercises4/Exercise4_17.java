9cc43151b7e20d634add94de6fe6d86f44999924
IsaHajj <abdulpr3@gmail.com>
2022-11-18 23:56:14 +0100

    First commit

---
+++ Exercises4/Exercise4_17.java
@@ -0,0 +1,12 @@
+/*
+(Days of a month) Write a program that prompts the user to enter a year and the
+first three letters of a month name (with the first letter in uppercase) and displays
+the number of days in the month.
+*/
+import java.util.Scanner;
+
+public class Exercise4_17 {
+   public static void main(String[] args) {
+      // Create a Scanner
+      Scanner input = new Scanner(System.in);
+
