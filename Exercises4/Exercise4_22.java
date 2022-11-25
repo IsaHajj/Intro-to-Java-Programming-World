e181b43c05253a357bd3fe9c8ffe034c71680025
IsaHajj <abdulpr3@gmail.com>
2022-11-25 23:59:29 +0100

    First commit

---
+++ Exercises4/Exercise4_22.java
@@ -0,0 +1,8 @@
+/*
+(Check SSN) Write a program that prompts the user to enter a Social Security
+number in the format DDD-DD-DDDD, where D is a digit. Your program should
+check whether the input is valid.
+*/
+import java.util.Scanner;
+
+public class Exercise4_21 {
