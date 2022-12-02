1d946183fdeadd319b197885b28391909bf0f8b0
IsaHajj <abdulpr3@gmail.com>
2022-12-02 23:59:25 +0100

    First commit

---
+++ Exercises5/Exercise5_01.java
@@ -0,0 +1,12 @@
+/*
+(Count positive and negative numbers and compute the average of numbers) Write
+a program that reads an unspecified number of integers, determines how many
+positive and negative values have been read, and computes the total and average of
+the input values (not counting zeros). Your program ends with the input 0. Display
+the average as a floating-point number.
+*/
+
+import java.util.Scanner;
+
+public class Exercise5_01 {
+   public static void main(String[] args) {
