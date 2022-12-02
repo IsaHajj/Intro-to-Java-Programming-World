a85df74b85a26f97585b105d03b1407e155a25a0
IsaHajj <abdulpr3@gmail.com>
2022-12-03 00:19:26 +0100

    First commit

--- Exercises5/Exercise5_01.java
+++ Exercises5/Exercise5_01.java
@@ -10,3 +10,44 @@ import java.util.Scanner;
 
 public class Exercise5_01 {
    public static void main(String[] args) {
+       // Create a Scanner
+       Scanner input = new Scanner(System.in);
+       // Count the number of positive numbers
+       int positives = 0; 
+       // Count the number of negative numbers	
+       int negatives = 0; 
+       // Count all numbers
+       int count = 0;	
+       // Accumulate a totol		
+       double total = 0;		
+
+
+       // Promopt the user to enter an integer or 0 to exit
+       System.out.print("Enter an integer, the input ends if it is 0: ");
+       int number = input.nextInt();
+       // Test for sentinel value
+       if (number == 0) {	
+	   System.out.println("No numbers are entered except 0");
+	   System.exit(1);
+       }
+
+       while (number != 0) {  // Test for sentinel value
+	  if (number > 0)
+	  positives++;	// Increase positives
+          else
+	  negatives++;  // Increase negatives
+	  total += number;  // Accumulate total
+	  count++;	    // Increase the count
+	  number = input.nextInt();
+       }
+
+       // Calculate the average
+       double average = total / count;
+
+       // Display results
+       System.out.println("The number of positive is " + positives +
+			  "\nThe number of negatives is " + negatives +
+			  "\nThe total is total " + total +
+			  "\nThe average is " + average);
+   }
+}
