0c92e11ee11d9db38fa3b0ebcfbd2506f73e6641
IsaHajj <abdulpr3@gmail.com>
2022-12-30 23:59:23 +0100

    Commited

---
+++ Exercises5/Exercise5_21.java
@@ -0,0 +1,12 @@
+/*
+(Financial application: compare loans with various interest rates) Write a program
+that lets the user enter the loan amount and loan period in number of years
+and displays the monthly and total payments for each interest rate starting from
+5% to 8%, with an increment of 1/8.
+*/
+import java.util.Scanner;
+
+public class Exercise5_21 {
+   public static void main(String[] args) {
+      // Create a Scanner
+      Scanner input = new Scanner(System.in);
