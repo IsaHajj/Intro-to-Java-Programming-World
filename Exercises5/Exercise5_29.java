a0ee9a6c972c71d6792312f7243ccc733c93de00
IsaHajj <abdulpr3@gmail.com>
2023-01-11 23:47:40 +0100

    Committed

--- Exercises5/Exercise5_29.java
+++ Exercises5/Exercise5_29.java
@@ -20,6 +20,7 @@ your program should display the calendar for each month in the year, as follows:
                       15  16  17  18  19  20  21
                       22  23  24  25  26  27  28
                       29  30  31
+                      
 */
 import java.util.Scanner;
 
