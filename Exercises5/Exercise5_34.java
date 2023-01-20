484808c8756b06934d1529bc3b2d6fc554ad1e30
IsaHajj <abdulpr3@gmail.com>
2023-01-20 23:12:53 +0100

    Committed

---
+++ Exercises5/Exercise5_34.java
@@ -0,0 +1,9 @@
+/*
+(Game: scissor, rock, paper) Programming Exercise 3.17 gives a program that
+plays the scissor-rock-paper game. Revise the program to let the user continuously
+play until either the user or the computer wins more than two times than its
+opponent.
+*/
+import java.util.Scanner;
+
+public class Exercise5_34 {
